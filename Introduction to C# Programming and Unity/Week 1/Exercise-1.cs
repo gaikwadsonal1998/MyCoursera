a6b56558f28a3a63f8ee9c8baa0c00ae583646dd
sonal <gaikwadsonal1998@gmail.com>
2020-09-12 14:20:07 +0530

    zip added

---
+++ Introduction to C# Programming and Unity/Week 1/Exercise-1.cs
@@ -0,0 +1,15 @@
+/*
+Problem 1 – Output your name
+Create a new C# Console Application named Exercise1.
+
+Add a line of code that will display your name to the user.
+*/
+
+using System;
+
+class Exercise1{
+	public static void Main(string[] args)
+	{
+		Console.WriteLine("Sonal");
+	}
+}
