9448fa03e33002dc6edb8baafd29f5c797578110
Sonal Gaikwad <gaikwadsonal1998@gmail.com>
2021-07-06 18:05:20 -0700

    QuizApp

---
+++ JAVASCRIPT/QuizApp/appScript.js
@@ -0,0 +1,13 @@
+var questions =[{
+	question: "What is your name?",
+	choices:["Sonal","Vishal","Prajakta","Snehal"],
+	correctAnswer:0
+},{
+	question: "What is your fav color?",
+	choices:["Red","Green","White","Violet"],
+	correctAnswer:2
+}];
+
+var currentQuestion=0;
+var correctAnswers=0;
+var quizOver=false;
